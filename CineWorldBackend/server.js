e2ace92ce1a49848dde1e7d9dc96e3180a2e6c21
jackaakash4 <jackaakash4@gmail.com>
2024-09-27 22:17:15 +0545

    Feat: Implement Rate Limiting

--- CineWorldBackend/server.js
+++ CineWorldBackend/server.js
@@ -1,9 +1,15 @@
 import express from "express"
 import cors from "cors"
 import dotenv from "dotenv"
+import rateLimit from 'express-rate-limit'
 
-dotenv.config()
 import connectDB from './Database.js'
+dotenv.config()
+
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000,
+	max: 100,
+});
 
 const app = express();
 const port = process.env.PORT;
@@ -11,7 +17,7 @@ const port = process.env.PORT;
 //Middleware
 app.use(cors());
 app.use(express.json());
-
+app.use(limiter);
 //database
 connectDB();
 
